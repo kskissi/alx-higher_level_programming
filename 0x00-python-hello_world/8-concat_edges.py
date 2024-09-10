d0323b33d7dbe43880605f93448beae17b8c74aa
kskissi <kwakusarfokissi.ksk@gmail.com>
2024-09-10 11:26:10 -0700

    8-concat_edges.py

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming\
-        language that combines remarkable power with very clear syntax"
-str = str[39:67] + str[107:112] + str[:6]
-print(str)
+
+text = (
+    "Python is an interpreted, interactive, object-oriented programming"
+    " language that combines remarkable power with very clear syntax"
+)
+
+text = text[39:67] + text[107:112] + text[:6]
+print(text)
 
